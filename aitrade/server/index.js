7a6eca02a69a62a6cb377b690cb3f3369738a081
btctExange <vincent.jaipul@gmail.com>
2019-12-03 14:52:39 +0800

    first commit

---
+++ aitrade/server/index.js
@@ -0,0 +1,17 @@
+const { GraphQLServer } = require('graphql-yoga')
+const resolvers = require('../resolvers')
+const { prisma } = require('../../aitrade-datamodel/aitrade/src/generated/prisma-client')
+
+const server = new GraphQLServer({
+  typeDefs: './server/schema.graphql',
+  resolvers,
+  context: req => ({
+    ...req,
+    prisma,
+  })
+})
+
+const options = { port: 4001 }
+server.start(options, ({ port }) => console.log('Server is running on http://localhost:4001'))
+
+
